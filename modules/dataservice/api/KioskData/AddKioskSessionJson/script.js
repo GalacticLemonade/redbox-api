8c7893ff09e1c8995d350e35b85ad3cc99180f7d
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-10 14:10:48 -0700

    implement proper response (i think?(

--- modules/dataservice/api/KioskData/AddKioskSessionJson/script.js
+++ modules/dataservice/api/KioskData/AddKioskSessionJson/script.js
@@ -5,12 +5,48 @@ import { fileURLToPath } from 'node:url';
 
 const appDir = dirname(fileURLToPath(import.meta.url));
 
+/*
+Sends:
+namespace Redbox.KioskDataServices.Proxy.ComponentModel
+{
+  public class AddKioskSessionRequest
+  {
+    public int KioskId { get; set; }
 
+    public string SessionId { get; set; }
+
+    public string StartTime { get; set; }
+
+    public string EndTime { get; set; }
+
+    public bool IsUnCompressed { get; set; }
+
+    public string SessionData { get; set; }
+  }
+}
+
+Expects:
+namespace Redbox.KioskDataServices.Proxy.ComponentModel
+{
+  public class KioskDataServicesResponse
+  {
+    public bool Success { get; set; }
+
+    public Dictionary<string, object> Data { get; set; }
+
+    public string ErrorMessage { get; set; }
+
+    public bool ExceptionRaised { get; set; }
+  }
+}
+*/
 
 export const execute = (req, res) => {
     res.status(200).json({
-        MessageId: v4(),
         Success: true,
+        Data: [ 'KioskID' = req.body.KioskId, 'SessionId' = req.body.SessionID ],
+        ErrorMessage: "",
+        ExceptionRaised: false
     })
 }
 
