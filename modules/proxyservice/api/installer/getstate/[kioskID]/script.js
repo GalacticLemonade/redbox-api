da4a1552207ba347378ea32032319a1554a8d48f
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-10 16:59:25 -0700

    document state

---
+++ modules/proxyservice/api/installer/getstate/[kioskID]/script.js
@@ -0,0 +1,21 @@
+import fs from 'node:fs';
+import { v4 } from "uuid"
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const appDir = dirname(fileURLToPath(import.meta.url));
+
+/*
+Outgoing:
+public short Id { get; set; }
+public string Description { get; set; }
+*/
+
+export const execute = (req, res) => {
+    res.status(200).json({
+        MessageId: v4(),
+        Success: true,
+    })
+}
+
+export const method = 'GET'
