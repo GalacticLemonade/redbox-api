26254539fdcdbad8e844a16a90d9459ee341bccd
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-09 18:04:54 -0700

    Add Reels server

---
+++ modules/reels/index.js
@@ -0,0 +1,37 @@
+// Reels Service
+
+import express from "express"
+import { v4 } from "uuid"
+import chalk from "chalk"
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = 3016
+const app = express();
+
+function log(message) {
+    console.log(chalk.green("modules/reels ") + chalk.green("INFO: ") + message)
+}
+
+function logWarning(message) {
+    console.log(chalk.green("modules/reels ") + chalk.yellow("WARNING: ") + message)
+}
+
+function logError(message) {
+    console.error(chalk.green("modules/reels ") + chalk.red("ERROR: ") + message)
+}
+
+export function run() {
+    app.use(express.json())
+
+    app.all("*", (req, res) => {
+
+    })
+
+    app.listen(PORT, () => {
+        log(`Server is listening on port ${PORT}!`);
+    });
+}
