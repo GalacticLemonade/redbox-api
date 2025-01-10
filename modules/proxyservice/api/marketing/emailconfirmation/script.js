bcc6aeeb407496f3f041c61b1078db20a3a42712
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-10 16:50:30 -0700

    document emailconfirmation

--- modules/proxyservice/api/marketing/emailconfirmation/script.js
+++ modules/proxyservice/api/marketing/emailconfirmation/script.js
@@ -7,9 +7,21 @@ const appDir = dirname(fileURLToPath(import.meta.url));
 
 /*
 Incoming:
+public Guid MessageId { get; set; }
+public long KioskId { get; set; }
+public string Email { get; set; }
+public string AccountNumber { get; set; }
+public DateTime TransactionDate { get; set; }
 
 Outgoing:
-
+public Guid MessageId { get; set; }
+public ConfirmationStatus ConfirmationStatus { get; set; }
+public string CustomerProfileNumber { get; set; }
+public string MobilePhoneNumber { get; set; }
+public bool PromptForPerks { get; set; }
+public bool PromptForEarlyId { get; set; }
+public bool TextClubEnabled { get; set; }
+public bool? EmailVerified { get; set; }
 */
 
 export const execute = (req, res) => {
