12073005d1393af348183d1980c2f484b72d6e78
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-10 14:11:18 -0700

    new endpoint

---
+++ modules/proxyservice/api/marketing/emailconfirmation/script.js
@@ -0,0 +1,23 @@
+import fs from 'node:fs';
+import { v4 } from "uuid"
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const appDir = dirname(fileURLToPath(import.meta.url));
+
+/*
+Sends:
+
+
+Expects:
+
+*/
+
+export const execute = (req, res) => {
+    res.status(200).json({
+        MessageId: v4(),
+        Success: true,
+    })
+}
+
+export const method = 'POST'
