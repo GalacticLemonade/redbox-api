2f904e7433f7112d7c1d12c0e1d515de8f39b4dd
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-09 18:05:01 -0700

    Add TransactionService

---
+++ modules/transactionservice/index.js
@@ -0,0 +1,37 @@
+// Transaction Service
+
+import express from "express"
+import { v4 } from "uuid"
+import chalk from "chalk"
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = 3015
+const app = express();
+
+function log(message) {
+    console.log(chalk.red("modules/transactionservice ") + chalk.green("INFO: ") + message)
+}
+
+function logWarning(message) {
+    console.log(chalk.red("modules/transactionservice ") + chalk.yellow("WARNING: ") + message)
+}
+
+function logError(message) {
+    console.error(chalk.red("modules/transactionservice ") + chalk.red("ERROR: ") + message)
+}
+
+export function run() {
+    app.use(express.json())
+
+    app.all("*", (req, res) => {
+
+    })
+
+    app.listen(PORT, () => {
+        log(`Server is listening on port ${PORT}!`);
+    });
+}
