c69b784c454678880b98857b7e484daeac961f8a
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-10 00:38:33 -0700

    documentation

--- modules/dataservice/api/KioskData/AddKioskSessionJson/script.js
+++ modules/dataservice/api/KioskData/AddKioskSessionJson/script.js
@@ -5,6 +5,18 @@ import { fileURLToPath } from 'node:url';
 
 const appDir = dirname(fileURLToPath(import.meta.url));
 
+/*
+Incoming:
+public int KioskId { get; set; }
+public string SessionId { get; set; }
+public string StartTime { get; set; }
+public string EndTime { get; set; }
+public bool IsUnCompressed { get; set; }
+public string SessionData { get; set; }
+
+Cannot find what it expects back
+*/
+
 export const execute = (req, res) => {
     res.status(200).json({
         MessageId: v4(),
