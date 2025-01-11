297a7d66a40c71d8ed91037814a6226dbe1e25c9
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-10 17:22:38 -0700

    implement base methods

---
+++ modules/proxyservice/api/customerprofile/login/script.js
@@ -0,0 +1,15 @@
+import fs from 'node:fs';
+import { v4 } from "uuid"
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const appDir = dirname(fileURLToPath(import.meta.url));
+
+export const execute = (req, res) => {
+    res.status(200).json({
+        MessageId: v4(),
+        Success: true,
+    })
+}
+
+export const method = 'POST'
