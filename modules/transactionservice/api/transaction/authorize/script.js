fe9569a1ef80f819755ea99195d218e0e601770f
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-10 17:05:51 -0700

    document authorize

--- modules/transactionservice/api/transaction/authorize/script.js
+++ modules/transactionservice/api/transaction/authorize/script.js
@@ -5,6 +5,32 @@ import { fileURLToPath } from 'node:url';
 
 const appDir = dirname(fileURLToPath(import.meta.url));
 
+/*
+Incoming:
+public DateTime TransactionDate { get; set; }
+public CreditCard CreditCard { get; set; }
+public ShoppingCart ShoppingCart { get; set; }
+public string CustomerProfileNumber { get; set; }
+public bool? AuthAcceptFlag { get; set; }
+public int? AuthRuleId { get; set; }
+public decimal? AuthAmount { get; set; }
+public bool? SkipAuthRule { get; set; }
+
+Outgoing:
+public Guid MessageId { get; set; }
+public string Email { get; set; }
+public long ReferenceNumber { get; set; }
+public string CustomerProfileLastName { get; set; }
+public string CustomerProfileFirstName { get; set; }
+public List<RentalLimit> Limits { get; set; }
+public bool HasProfile { get; set; }
+public string CustomerProfileNumber { get; set; }
+public ConfirmationStatus ConfirmationStatus { get; set; }
+public string AccountNumber { get; set; }
+public bool KioskTransactionSuccess { get; set; }
+public bool SubscriptionTransactionSuccess { get; set; }
+*/
+
 export const execute = (req, res) => {
     res.status(200).json({
         MessageId: v4(),
