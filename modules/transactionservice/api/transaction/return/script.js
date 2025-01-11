72a49b77676f6a718d85695666454f89d2f36e1d
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-10 17:10:47 -0700

    document more transaction stuff

--- modules/transactionservice/api/transaction/return/script.js
+++ modules/transactionservice/api/transaction/return/script.js
@@ -5,6 +5,19 @@ import { fileURLToPath } from 'node:url';
 
 const appDir = dirname(fileURLToPath(import.meta.url));
 
+/*
+Incoming:
+public byte ReturnType { get; set; }
+public string Barcode { get; set; }
+public short Deck { get; set; }
+public short Slot { get; set; }
+public DateTime ReturnDate { get; set; }
+public bool FailedSecurityRead { get; set; }
+
+Outgoing:
+BaseResponse
+*/
+
 export const execute = (req, res) => {
     res.status(200).json({
         MessageId: v4(),
