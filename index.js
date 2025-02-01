1d4d9cea2c26e70b6a913c36f11dbc4126d36dcf
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-31 23:49:30 -0700

    fix eslint

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-import path, { dirname } from 'path'
-import { spawn } from 'child_process'
-import http from 'http'
-import { Server } from "socket.io";
+import path, { dirname } from 'path';
+import { spawn } from 'child_process';
+import http from 'http';
+import { Server } from 'socket.io';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 //http server for socket.io
-const server = http.createServer()
+const server = http.createServer();
 
 //http port
-const port = 8005
+const port = 8005;
 
 //attach socket.io to server
-const ioServer = new Server(server)
+const ioServer = new Server(server);
 
 //listen for client connections
 ioServer.on('connection', (socket) => {
-    console.log('Service connected');
+	console.log('Service connected');
 
-    //lsten for a message from the client
-    socket.on('message', (data) => {
-        console.log('Received from service:', data);
-    });
+	//lsten for a message from the client
+	socket.on('message', (data) => {
+		console.log('Received from service:', data);
+	});
 
-    //handle client disconnect
-    socket.on('disconnect', () => {
-        console.log('Service disconnected');
-    });
+	//handle client disconnect
+	socket.on('disconnect', () => {
+		console.log('Service disconnected');
+	});
 });
 
 //start microservice function
 const startMicroservice = (serviceName) => {
-    const servicePath = path.join(__dirname, 'services', serviceName, 'index.js')
-    console.log(servicePath)
-    const child = spawn('node', [servicePath])
+	const servicePath = path.join(__dirname, 'services', serviceName, 'index.js');
+	console.log(servicePath);
+	const child = spawn('node', [servicePath]);
 
-    child.stdout.on('data', (data) => {
-        console.log(`${serviceName}: ${data}`)
-    })
+	child.stdout.on('data', (data) => {
+		console.log(`${serviceName}: ${data}`);
+	});
 
-    child.stderr.on('data', (data) => {
-        console.error(`${serviceName}: ${data}`)
-    })
+	child.stderr.on('data', (data) => {
+		console.error(`${serviceName}: ${data}`);
+	});
 
-    child.on('close', (code) => {
-        console.log(`${serviceName} exited with code ${code}`)
-    })
+	child.on('close', (code) => {
+		console.log(`${serviceName} exited with code ${code}`);
+	});
 };
 
 //listen on http
-server.listen(port)
+server.listen(port);
 
 //start all services
-startMicroservice("db")
-startMicroservice("utils")
-startMicroservice("testservice")
+startMicroservice('db');
+startMicroservice('utils');
+startMicroservice('testservice');
